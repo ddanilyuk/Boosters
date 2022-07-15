3b0d7d316aeeff57ad419649ace9cf0096591f74
denis.danyliuk <danis.danilyuk@gmail.com>
2022-07-15 16:05:13 +0300

    Initial Commit

---
+++ Boosters/BoostersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoostersApp.swift
+//  Boosters
+//
+//  Created by Denys Danyliuk on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct BoostersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
